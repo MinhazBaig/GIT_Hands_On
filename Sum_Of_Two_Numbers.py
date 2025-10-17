fb8c75ce17b4cc6902ab849233396d4acee9b155
Minhaz Baig Mirza <minhaz.baig@gmail.com>
2025-10-17 10:10:50 +0530

    minor changes
    new file updation

--- Sum_Of_Two_Numbers.py
+++ Sum_Of_Two_Numbers.py
@@ -1,4 +1,5 @@
 num1 = 10
 num2 = 25
-sum_result = num1 + num2
-print(f"The sum of {num1} and {num2} is: {sum_result}")
+num3 =35
+sum_result = num1 + num2 + num3
+print(f"The sum of {num1} {num2} and {num3} is: {sum_result}")
